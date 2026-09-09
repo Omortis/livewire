70ff1a66f5cfca0fd3e418a868163ae441b70415
John Bleichert <omortis@outlook.com>
2026-09-09 08:05:41 -0400

    WIP: Java Exercise 1.6 NIO File Walker (in progress)

--- java/exercises/src/main/java/com/livewire/App.java
+++ java/exercises/src/main/java/com/livewire/App.java
@@ -1,57 +1,21 @@
 package com.livewire;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-
-class Equipment {
-    public String id;
-    public String type;
-    public Double voltage_kv;
-
-    public Equipment(String id, String type, Double voltage) {
-        this.id = id;
-        this.type = type;
-        this.voltage_kv = voltage;
-    }
-
-    public String toString() {
-        return "id = " + id + ", type = " + type + ", voltage_kv = " + voltage_kv;
-    }
-}
-
-class EquipmentRegistry<T> {
-    List<T> registry = new ArrayList<>();
-
-    public void add(T item) {
-        registry.add(item);
-    }
-    
-    public List<T> findBy(Predicate<T> filter) {
-        List<T> result = new ArrayList<>();
-        for (T item : registry) {
-            if (filter.test(item)) {
-                result.add(item);
-            }
-        }
-        return result;
-    }
-}
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        EquipmentRegistry<Equipment> registry = new EquipmentRegistry<>();
-
-        registry.add(new Equipment("eq1", "thermal", 55.0));
-        registry.add(new Equipment("eq2", "coal", 60.0));
-        registry.add(new Equipment("eq3", "solar", 40.0));
-        registry.add(new Equipment("eq4", "nuclear", 70.0));
-        registry.add(new Equipment("eq5", "wind", 30.0));
-
-        List<Equipment> highVoltage = registry.findBy(e -> e.voltage_kv > 50);
-
-        for (Equipment equipment : highVoltage) {
-            System.out.println(equipment);
+        if (args.length == 0) {
+            System.err.println("Usage: java App <directory>");
+            return;
         }
+        
+        // TODO: Files.walk() with try-with-resources
+        // TODO: Filter for .log files
+        // TODO: For each file, Files.lines() and count "ALARM"
+        // TODO: Sum across all files and print total
     }
 }
